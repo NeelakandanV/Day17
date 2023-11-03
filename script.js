e21643731f3a592cb15d849d140dc701f3152029
NeelakandanV <hari0709venkatesan@gmail.com>
2023-11-03 17:21:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+var bodyele = document.querySelector('body')
+let cdn = document.createElement('h1')
+bodyele.append(cdn)
+setTimeout(()=>{
+    setTimeout(()=>{
+        setTimeout(()=>{
+            setTimeout(()=>{
+                setTimeout(()=>{
+                    setTimeout(()=>{
+                        setTimeout(()=>{
+                            setTimeout(()=>{
+                                setTimeout(()=>{
+                                    setTimeout(()=>{
+                                        setTimeout(()=>{
+                                            setTimeout(()=>{
+                                                cdn.innerText="Happy Independence Day";
+                                                bodyele.style.backgroundColor="dimgrey";
+                                            },300)
+                                            cdn.innerText = '00:00:00';
+                                            bodyele.style.backgroundColor="green";     
+                                        },1000)
+                                        cdn.innerText = '00:00:01';
+                                        bodyele.style.backgroundColor="white";  
+                                    },1000)
+                                    cdn.innerText = '00:00:02';
+                                    bodyele.style.backgroundColor="orange";
+                                },1000)
+                                cdn.innerText = '00:00:03'; 
+                                bodyele.style.backgroundColor="indigo";    
+                            },1000)
+                            cdn.innerText = '00:00:04'; 
+                             bodyele.style.backgroundColor="gold";
+                        },1000)
+                        cdn.innerText = '00:00:05';
+                        bodyele.style.backgroundColor="aqua";
+                    },1000)
+                    cdn.innerText = '00:00:06';
+                    bodyele.style.backgroundColor="chocolate";
+                },1000)
+                cdn.innerText = '00:00:07';
+                 bodyele.style.backgroundColor="violet";
+            },1000)
+            cdn.innerText = '00:00:08';
+            bodyele.style.backgroundColor="chartreuse";
+        },1000)
+        cdn.innerText = '00:00:09';
+        bodyele.style.backgroundColor="blue";
+    },1000)
+    cdn.innerText = '00:00:10';
+    bodyele.style.backgroundColor="red";
+},1000)
